e03533d52fba6c2bf5774853618cb1e7d583f115
Karim <aarrale12@gmail.com>
2022-06-02 18:11:10 -0500

    added method to choos game type

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+from tarfile import _Bz2ReadableFileobj
 from human import Human
 from ai import Ai
 
@@ -30,6 +31,31 @@ class Game:
         print('Lizard eats Paper')
         print('Paper disproves Spock')
         print('Spock vaporizes spock')
+
+    def choose_game_type(self):
+        while True:
+            try:
+                game_type = int(input('Choose 1 for player vs player or choose 2 for player vs ai'))
+            except ValueError:
+                    continue
+            if game_type == 1:
+                self.player2 = Human()
+                self.player1.get_name()
+                print(f'Hi{self.player1.name}.')
+                self.player2.get_name()
+                print(f'Hi {self.player2.name}')
+                print(f'{self.player1.name} vs {self.player2.name}')
+                break
+            elif game_type == 2:
+                self.player2 = Ai()
+                self.player1.get_name()
+                print(f'Hi{self.player1.name}')
+                break
+
+    def
+
+
+
 
 
 
