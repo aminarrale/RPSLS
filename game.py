6647b5b8df9adc0c1f2da11bad21b3a3b71fa508
Karim <aarrale12@gmail.com>
2022-06-02 15:51:25 -0500

    added game rules

--- game.py
+++ game.py
@@ -1,11 +1,39 @@
+from human import Human
+from ai import Ai
+
+
 class Game:
     def __init__(self):
+        self.player1 = Human()
+        self.player2 = None
+    
+    def run_game(self):
+        self.display_greeting()
+        self.game_rules()
+
         
     
     
     def display_greeting(self):
-        print('Welcome to Rock Paper Scissors Lizard Spock')
-        print('Heres the rules to the game')
+        print('Welcome to Rock Paper Scissors Lizard Spock.')
+        print('each match will be the best of three games')
+
+
+    def game_rules(self):
+        print('Here are the rules to the game:')
         print('Rock crushes Scissors')
         print('Scissors cuts Paper')
+        print('Paper covers Rock')
+        print('Rock crushes Lizard')
+        print('Lizard poisons Spock')
+        print('Spock smashes Scissors')
+        print('Lizard eats Paper')
+        print('Paper disproves Spock')
+        print('Spock vaporizes spock')
+
+
+
+
+
+    
 
